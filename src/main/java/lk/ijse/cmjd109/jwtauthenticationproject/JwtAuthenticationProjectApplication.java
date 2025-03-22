e1c21a347614f37c09e120be41e9ddd06cbbee1c
Dassana <dassana002@gmail.com>
2025-03-22 18:01:32 +0530

    first Commit

---
+++ src/main/java/lk/ijse/cmjd109/jwtauthenticationproject/JwtAuthenticationProjectApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd109.jwtauthenticationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthenticationProjectApplication.class, args);
+    }
+
+}
