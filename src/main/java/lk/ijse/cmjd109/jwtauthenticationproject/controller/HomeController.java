942d2d98480bf8d0d74a17b889b6661deb45262d
Dassana <dassana002@gmail.com>
2025-03-24 19:26:52 +0530

    Sovled the Unathourized 401 Error

---
+++ src/main/java/lk/ijse/cmjd109/jwtauthenticationproject/controller/HomeController.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd109.jwtauthenticationproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/test")
+    public String home() {
+        return "Hello World";
+    }
+}
