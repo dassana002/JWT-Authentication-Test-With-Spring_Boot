b85d6a3cdc9079abbe6b0177ce7878b065860a7a
Dassana <dassana002@gmail.com>
2025-03-29 17:52:31 +0530

    User input a Password for Custom Authentication Provider Created

--- src/main/java/lk/ijse/cmjd109/jwtauthenticationproject/config/SecurityConfig.java
+++ src/main/java/lk/ijse/cmjd109/jwtauthenticationproject/config/SecurityConfig.java
@@ -2,15 +2,19 @@ package lk.ijse.cmjd109.jwtauthenticationproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -24,4 +28,23 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
+
+    // Custom Authentication Provider Generated
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        
+        return provider;
+    }
+
+    // Custom Password Encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);  // password eke tiyena characters ganana 12 ta wadinm hodai
+    }
+
+
 }
+
+
