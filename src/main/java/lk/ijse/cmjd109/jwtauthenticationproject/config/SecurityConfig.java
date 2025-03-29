a1f02f7bb44ae75be4fc0d26dcaedc9b629daa6e
Dassana <dassana002@gmail.com>
2025-03-29 14:44:18 +0530

    Custom Configurations Add to SecurityConfiguration Class

--- src/main/java/lk/ijse/cmjd109/jwtauthenticationproject/config/SecurityConfig.java
+++ src/main/java/lk/ijse/cmjd109/jwtauthenticationproject/config/SecurityConfig.java
@@ -2,15 +2,26 @@ package lk.ijse.cmjd109.jwtauthenticationproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return null;
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf->
+                        csrf.disable())
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(r->
+                        r.requestMatchers("/login").permitAll().anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
--- src/main/java/lk/ijse/cmjd109/jwtauthenticationproject/controller/HomeController.java
+++ src/main/java/lk/ijse/cmjd109/jwtauthenticationproject/controller/HomeController.java
@@ -1,6 +1,7 @@
 package lk.ijse.cmjd109.jwtauthenticationproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HomeController {
     @GetMapping("/test")
     public String home() {
         return "Hello World";
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        return "Hello Login";
     }
 }
