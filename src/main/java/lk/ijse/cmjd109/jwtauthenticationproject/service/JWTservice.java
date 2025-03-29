8641e93a51ee0ec5879903432b4467567656cf37
Dassana <dassana002@gmail.com>
2025-03-29 15:29:51 +0530

    JWT Token Generated

--- src/main/java/lk/ijse/cmjd109/jwtauthenticationproject/service/JWTservice.java
+++ src/main/java/lk/ijse/cmjd109/jwtauthenticationproject/service/JWTservice.java
@@ -1,11 +1,14 @@
 package lk.ijse.cmjd109.jwtauthenticationproject.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +16,9 @@ public class JWTservice {
 
     private final SecretKey secretKey;
 
+    //-----------------------Secret Key Generated--------------------------
+
+    // "HmacSHA256" this algorithm used
     public JWTservice() {
         try {
             SecretKey k = KeyGenerator.getInstance("HmacSHA256").generateKey();
@@ -21,4 +27,22 @@ public class JWTservice {
             throw new RuntimeException(e);
         }
     }
+
+
+    // -------------------------- JWT Token Generated --------------------------
+
+    // subject( username)
+    // issuedAt( today time eka set kranwa)
+    // expiration( apita kemathi widiyata time eka denna puluwan (15 minitues) )
+    // signWith( api generate kragatta Secret Key eka)
+    public String generateJWTToken() {
+        return Jwts.builder()
+                .subject("dassana")
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+                .signWith(secretKey)
+                .compact();
+    }
+
+
 }
